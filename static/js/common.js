c7d54c577b488fdfebfcb59a411e1bb475bcde70
macoli <1148681396@qq.com>
2017-02-13 21:04:07 +0800

    update

--- static/js/common.js
+++ static/js/common.js
@@ -1,13 +1,13 @@
 /**
  * Created by Administrator on 2016/12/22.
  */
-num = 0;  //全局变量：0代表非编辑模式  1代表编辑模式
+//num = 0;  //全局变量：0代表非编辑模式  1代表编辑模式
 
 //全选
 function SelectAll(ths) {
-    if(num == 0) {
+    num = $(ths).parent().attr('num');
+    if(num == '0') {
         $(ths).parent().next().find("input[type='checkbox']").prop('checked',true);
-        console.log($(ths).parent().next().find("input[type='checkbox']"));
     } else {
         $(ths).parent().next().find("input[type='checkbox']").each(function () {
             //判断是否选中，选中则取消；没有则选中
@@ -17,11 +17,11 @@ function SelectAll(ths) {
          });
         $(ths).parent().next().find("input[type='checkbox']").prop('checked',true);
     }
-
 }
 //取消
 function CancelAll(ths) {
-    if(num == 0){
+    num = $(ths).parent().attr('num');
+    if(num == "0"){
         $(ths).parent().next().find("input[type='checkbox']").prop('checked',false);
     } else {
         //循环所有input标签
@@ -32,11 +32,11 @@ function CancelAll(ths) {
         });
         $(ths).parent().next().find("input[type='checkbox']").prop('checked',false);
     }
-
 }
 //反选
 function ReverseAll(ths) {
-    if(num == 0) {
+    num = $(ths).parent().attr('num');
+    if(num == "0") {
         //循环所有input标签
         $(ths).parent().next().find("input[type='checkbox']").each(function () {
             //判断是否选中，选中则取消；没有则选中
@@ -63,21 +63,21 @@ function ReverseAll(ths) {
 
 //编辑模式切换
 function ChangeMode(ths) {
+    num = $(ths).parent().attr('num');
     //0代表非编辑模式  1代表编辑模式
-    if(num == 0) {  //原来是非编辑模式，点击后变成编辑模式
-        num = 1;
-//                console.log('编辑模式');
+    if(num == "0") {  //原来是非编辑模式，点击后变成编辑模式
+        $(ths).parent().attr('num', "1");
         $(ths).addClass('editmode');  //变换成编辑模式的样式
         //循环所有input标签
         $(ths).parent().next().find("input[type='checkbox']").each(function () {
             //判断是否选中，选中则取消；没有则选中
-            if($(this).prop('checked')) {
+            if($(this).prop('checked') && $(this).parent().parent().find("a").eq(0).text() == '编辑') {
                 OnEdit(this);  //进入编辑模式
             }
          });
 
     } else { //原来是编辑模式，点击后变成非编辑模式
-        num = 0;
+        $(ths).parent().attr('num', "0");
 //                console.log('非编辑模式');
         $(ths).removeClass('editmode');  //删除编辑模式的样式
         //循环所有input标签
@@ -91,6 +91,7 @@ function ChangeMode(ths) {
 
 //进入编辑模式
 function OnEdit(ths) {
+    $(ths).parent().parent().find('a').eq(0).text('保存');
     //将内容变为文本输入框
     $(ths).parent().parent().find('.edit_input').each(function () {//找到所有需要变成文本输入框的标签，然后遍历
 //                            console.log($(this).text());
@@ -120,6 +121,7 @@ function OnEdit(ths) {
 
 //退出编辑模式
 function OffEdit(ths) {
+    $(ths).parent().parent().find('a').eq(0).text('编辑');
     //将文本输入框变为普通内容
     $(ths).parent().parent().find('.edit_input').each(function () {//找到所有需要变成普通内容的标签，然后遍历
         text_input = $(this).find('input').val();  //获取input标签的内容
@@ -136,13 +138,49 @@ function OffEdit(ths) {
 
 //编辑模式下，选中或取消checkbox也能进入退出编辑状态
 $('table input[type="checkbox"]').click(function () {
+    num = $(this).parent().parent().parent().parent().prev().attr('num');
     //编辑模式下，点击CheckBox也能进入退出编辑模式
-    if(num == 1) {
+    if(num == "1") {
         if($(this).prop('checked')) {  //选中，进入编辑模式
-//                    console.log($(this));
             OnEdit(this);
         } else {  //取消选中，退出编辑模式
             OffEdit(this);
         }
+    } else if(num == '0' && $(this).parent().parent().find('a').eq(0).text() == '保存') {
+        if($(this).prop('checked') == false) {
+            OffEdit(this);
+        }
     }
 });
+
+
+//批量删除
+function DeleteSelect(ths) {
+    $(ths).parent().next().find("input[type='checkbox']").each(function () {
+        //判断是否选中，选中则删除
+        if($(this).prop('checked')) {
+            $(this).parent().parent().remove();
+        }
+     });
+}
+
+
+//编辑单行数据
+function Edit(ths) {
+    if($(ths).text() == '编辑') {
+        if($(ths).parent().parent().find('input[type="checkbox"]').prop('checked') == false) {
+            $(ths).parent().parent().find('input[type="checkbox"]').prop('checked', true);
+        }
+        OnEdit(ths);
+    } else {
+        if($(ths).parent().parent().find('input[type="checkbox"]').prop('checked')) {
+            $(ths).parent().parent().find('input[type="checkbox"]').prop('checked', false)
+        }
+        OffEdit(ths);
+    }
+}
+
+//删除单行数据
+function Delete(ths) {
+    $(ths).parent().parent().remove();
+}
