90de8585e1c17e12fd9625855384a83cdc5a4a30
macoli <1148681396@qq.com>
2017-02-14 21:02:00 +0800

    update

--- app01/views.py
+++ app01/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from app01 import models
+import json
 
 
 # Create your views here.
@@ -24,19 +25,43 @@ def index(request):
     :param request:
     :return:
     """
-    user_info = {'user': None, 'message': {}}
-    user = request.session.get('user', None)
-    user_info['user'] = user
-    user_obj = models.User.objects.get(user=user)
-    group_objs = user_obj.group.all()
-    # print(group_objs)
-    for group in group_objs:
-        host_objs = group.host_set.all()
-        user_info['message'][group.name] = []
-        for host in host_objs:
-            user_info['message'][group.name].append(host)
-    # print(user_info)
-    return render(request, 'index.html', user_info)
+    if request.POST:
+        ret = {'status': False, 'id':None}
+        status = request.POST.get('status')
+        group_name = request.POST.get('group_name')
+        host_id = request.POST.get('id')
+        hostname = request.POST.get('hostname')
+        ip = request.POST.get('ip')
+        port = request.POST.get('port')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        if status == 'edit':  # 修改数据
+            if host_id:  # 有id，修改数据
+                models.Host.objects.filter(id=host_id).update(hostname=hostname, ip=ip, port=port, username=username, password=password)
+                ret['status'] = True
+            else:  # 无id，添加数据
+                models.Host.objects.create(hostname=hostname, ip=ip, port=port, username=username, password=password, group=models.HostGroup.objects.get(name=group_name))
+                id = models.Host.objects.get(hostname=hostname, ip=ip, port=port, username=username, password=password).id
+                ret['id'] = id
+                ret['status'] = True
+        elif status == 'delete':  # 删除数据
+            models.Host.objects.filter(id=host_id).delete()
+            ret['status'] = True
+        return HttpResponse(json.dumps(ret))
+    else:
+        user_info = {'user': None, 'message': {}}
+        user = request.session.get('user', None)
+        user_info['user'] = user
+        user_obj = models.User.objects.get(user=user)
+        group_objs = user_obj.group.all()
+        # print(group_objs)
+        for group in group_objs:
+            host_objs = group.host_set.all()
+            user_info['message'][group.name] = []
+            for host in host_objs:
+                user_info['message'][group.name].append(host)
+        # print(user_info)
+        return render(request, 'index.html', user_info)
 
 
 def login(request):
--- static/js/common.js
+++ static/js/common.js
@@ -1,122 +1,19 @@
 /**
  * Created by Administrator on 2016/12/22.
  */
-//num = 0;  //全局变量：0代表非编辑模式  1代表编辑模式
-
-//全选
-function SelectAll(ths) {
-    num = $(ths).parent().attr('num');
-    if(num == '0') {
-        $(ths).parent().next().find("input[type='checkbox']").prop('checked',true);
-    } else {
-        $(ths).parent().next().find("input[type='checkbox']").each(function () {
-            //判断是否选中，选中则取消；没有则选中
-            if($(this).prop('checked') == false) {
-                OnEdit(this);  //进入编辑模式
-            }
-         });
-        $(ths).parent().next().find("input[type='checkbox']").prop('checked',true);
-    }
-}
-//取消
-function CancelAll(ths) {
-    num = $(ths).parent().attr('num');
-    if(num == "0"){
-        $(ths).parent().next().find("input[type='checkbox']").prop('checked',false);
-    } else {
-        //循环所有input标签
-        $(ths).parent().next().find("input[type='checkbox']").each(function () {
-            if($(this).prop('checked')) {
-                OffEdit(this);//退出编辑模式
-            }
-        });
-        $(ths).parent().next().find("input[type='checkbox']").prop('checked',false);
-    }
-}
-//反选
-function ReverseAll(ths) {
-    num = $(ths).parent().attr('num');
-    if(num == "0") {
-        //循环所有input标签
-        $(ths).parent().next().find("input[type='checkbox']").each(function () {
-            //判断是否选中，选中则取消；没有则选中
-            if ($(this).prop('checked')) {
-                $(this).prop('checked', false);
-            } else {
-                $(this).prop('checked', true);
-            }
-        })
-    } else {
-        //循环所有input标签
-        $(ths).parent().next().find("input[type='checkbox']").each(function () {
-            //判断是否选中，选中则取消；没有则选中
-            if ($(this).prop('checked')) {
-                OffEdit(this);//退出编辑模式
-                $(this).prop('checked', false);
-            } else {
-                $(this).prop('checked', true);
-                OnEdit(this);  //进入编辑模式
-            }
-        })
-    }
-}
-
-//编辑模式切换
-function ChangeMode(ths) {
-    num = $(ths).parent().attr('num');
-    //0代表非编辑模式  1代表编辑模式
-    if(num == "0") {  //原来是非编辑模式，点击后变成编辑模式
-        $(ths).parent().attr('num', "1");
-        $(ths).addClass('editmode');  //变换成编辑模式的样式
-        //循环所有input标签
-        $(ths).parent().next().find("input[type='checkbox']").each(function () {
-            //判断是否选中，选中则取消；没有则选中
-            if($(this).prop('checked') && $(this).parent().parent().find("a").eq(0).text() == '编辑') {
-                OnEdit(this);  //进入编辑模式
-            }
-         });
-
-    } else { //原来是编辑模式，点击后变成非编辑模式
-        $(ths).parent().attr('num', "0");
-//                console.log('非编辑模式');
-        $(ths).removeClass('editmode');  //删除编辑模式的样式
-        //循环所有input标签
-        $(ths).parent().next().find("input[type='checkbox']").each(function () {
-            if($(this).prop('checked')) {
-                OffEdit(this);//退出编辑模式
-            }
-        });
-    }
-}
 
 //进入编辑模式
 function OnEdit(ths) {
     $(ths).parent().parent().find('a').eq(0).text('保存');
     //将内容变为文本输入框
     $(ths).parent().parent().find('.edit_input').each(function () {//找到所有需要变成文本输入框的标签，然后遍历
-//                            console.log($(this).text());
         var t = document.createElement('input'); //创建input标签
-        t.setAttribute('type','text'); //深圳type为text
+        t.setAttribute('type','text'); //设置type为text
+        t.className = 'edit';
         t.value = $(this).text();  //将原来标签的值赋给input标签
         $(this).empty(); //清空原标签的内容
         $(this).html(t); //将input标签插入
     });
-
-    //将内容变为下拉选择框
-    $(ths).parent().parent().find('.edit_select').each(function () {
-//                            console.log($(this).text());
-        var text = $(this).text();  //获取标签的内容
-        var select = document.createElement('select');  //创建select标签
-        $(select).html("<option>在线</option><option>下线</option>");  //select标签中添加option选项
-        //原来td标签中的值是什么，就选中哪个option标签
-        $(select).find('option').each(function () {
-           if($(this).text() == text) {
-               $(this).attr('selected',true);
-           }
-        });
-        $(this).empty();  //清空td标签
-        $(this).html(select);  //td标签中添加select标签
-    })
 }
 
 //退出编辑模式
@@ -128,59 +25,72 @@ function OffEdit(ths) {
         $(this).empty(); //清空td标签
         $(this).text(text_input);  //将文本框换位普通内容
     });
-    //将下拉选择框变为内容
-    $(ths).parent().parent().find('.edit_select').each(function () {
-        text_select = $(this).find('option:selected').val();//获取当前下拉框选中的值
-        $(this).empty();//清空td标签
-        $(this).text(text_select);//将select下拉框位普通内容
+}
+
+function Submit(ths, status, group_name) {
+    //获取当前行数据，提交给后台
+    var id = $(ths).parent().parent().find('td').eq(0).text();
+    var hostname = $(ths).parent().parent().find('td').eq(1).text();
+    var ip = $(ths).parent().parent().find('td').eq(2).text();
+    var port = $(ths).parent().parent().find('td').eq(3).text();
+    var username = $(ths).parent().parent().find('td').eq(4).text();
+    var password = $(ths).parent().parent().find('td').eq(5).text();
+
+    var csrftoken = $.cookie('csrftoken'); //去cookie中获取csrftoken的值
+                    function csrfSafeMethod(method) {
+                        // these HTTP methods do not require CSRF protection
+                        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+                    }
+                    $.ajaxSetup({
+                        beforeSend: function(xhr, settings) {
+                            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+                            }
+                        }
+                    });
+
+    $.ajax({
+        url: '/index/',
+        type: "post",
+        data: {'status': status, 'group_name': group_name, 'id': id, 'hostname': hostname, 'ip': ip, 'port': port, 'username': username, 'password': password},
+        dataType: 'json',
+        success: function (data) {
+            if(data.status){
+                //console.log('提交成功');
+                if(data.id) {
+                    $(ths).parent().parent().find('td').eq(0).text(data.id);
+                }
+            }else {
+                alert('数据保存失败！');
+            }
+        }
     })
 }
-
-//编辑模式下，选中或取消checkbox也能进入退出编辑状态
-$('table input[type="checkbox"]').click(function () {
-    num = $(this).parent().parent().parent().parent().prev().attr('num');
-    //编辑模式下，点击CheckBox也能进入退出编辑模式
-    if(num == "1") {
-        if($(this).prop('checked')) {  //选中，进入编辑模式
-            OnEdit(this);
-        } else {  //取消选中，退出编辑模式
-            OffEdit(this);
-        }
-    } else if(num == '0' && $(this).parent().parent().find('a').eq(0).text() == '保存') {
-        if($(this).prop('checked') == false) {
-            OffEdit(this);
-        }
-    }
-});
-
-
-//批量删除
-function DeleteSelect(ths) {
-    $(ths).parent().next().find("input[type='checkbox']").each(function () {
-        //判断是否选中，选中则删除
-        if($(this).prop('checked')) {
-            $(this).parent().parent().remove();
-        }
-     });
-}
-
 
 //编辑单行数据
 function Edit(ths) {
     if($(ths).text() == '编辑') {
-        if($(ths).parent().parent().find('input[type="checkbox"]').prop('checked') == false) {
-            $(ths).parent().parent().find('input[type="checkbox"]').prop('checked', true);
-        }
         OnEdit(ths);
     } else {
-        if($(ths).parent().parent().find('input[type="checkbox"]').prop('checked')) {
-            $(ths).parent().parent().find('input[type="checkbox"]').prop('checked', false)
-        }
+        //退出编辑模式
         OffEdit(ths);
+        var group_name = $(ths).parent().parent().parent().parent().parent().parent().parent().prev().find('a').eq(0).text();
+        Submit(ths, 'edit',group_name)
     }
 }
 
 //删除单行数据
 function Delete(ths) {
     $(ths).parent().parent().remove();
+    var group_name = $(ths).parent().parent().parent().parent().parent().parent().parent().prev().find('a').eq(0).text();
+    Submit(ths, 'delete', group_name);
+}
+
+//添加数据
+function Add(ths) {
+    var tr = $(ths).parent().parent().parent().next().find('tr').eq(0).clone();  //复制表格一行数据
+    tr.find('td').eq(0).empty();
+    tr.find('.edit_input').empty();
+    $(ths).parent().parent().parent().next().append(tr); //表格追加数据
+    OnEdit($(ths).parent().parent().parent().next().find('a').last());   //复制的数据改为编辑模式
 }
